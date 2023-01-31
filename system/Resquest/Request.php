badc1a2160afea58c82ca0b593baec3879435ee8
bab882 <chanitebrandon@gmail.com>
2023-01-31 09:13:48 +0100

    first commit

--- system/Resquest/Request.php
+++ system/Resquest/Request.php
@@ -34,21 +34,51 @@ class Request
 
     protected function run($rules)
     {
+        // faire l'ensemble de l'upload de l'image
+        foreach($rules as $att => $values)
+        {
+            // Nous allons séparer les champs par le pipe
+            $ruleArray = explode('|', $values);
+            if(in_array('file', $ruleArray))
+            {
+                // Nous allons détruire les éléments dans le tableau
 
+                // Je vais rechercher dans le tableau
+                unset($ruleArray[array_search('file', $ruleArray)]);
+
+                //Nous allons passer dans une nouvelle fonction pour la validation
+                $this->fileValidation($att, $ruleArray);
+
+            }
+            // Pour la validation des nombres (peut importe l'origin du nombre)
+            elseif(in_array('number', $ruleArray))
+            {
+                $this->numberValidation($att, $ruleArray);
+            }
+            else
+            {
+                // Validation dite normal
+                $this->normalValidation($att, $ruleArray);
+            }
+        }
     }
 
     protected function file($name)
     {
-
+        return isset($this->files[$name]) ? $this->files[$name] : false;
     }
 
     protected function postAttributes()
     {
-
+        foreach($_POST as $key => $value)
+        {
+            $this->$key = htmlentities($value);
+            $this->request[$key] = htmlentities($value);        
+        }
     }
 
     public function all()
     {
-        
+        return $this->request;
     }
 }
